532420c1c171a4652bb12c7caa4f484e7dea7d56
Léonard <91879922+leonarddomin@users.noreply.github.com>
2022-05-02 13:02:45 +0200

    JS

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,17 +1,35 @@
+// Déclaration d'une instance de Vue //
 const myCalc = new Vue({
     el: '#template',
     data: {
+        //
         number : "0" 
     }, 
+// Déclaration de la Méthode //
     methods: {
+    
         print(nombre) {
-            this.number += nombre
+            if (this.number == "0") {
+                this.number = ""
+                this.number += nombre
+            } 
+            else {
+                this.number += nombre
+            } 
+            console.log(this.number)
         },
+// Pour Reset la Calculette //
        clear: function() {
-           this.number = ""
+           this.number = "0"
        },
-      somme: function() {
-          
-      }
+// Pour afficher le résultat //
+      results: function() {
+          this.number = eval(this.number)
+      },
+// Pourcentage //
+      pourcent: function() {
+          this.number = (this.number/100)
+      }, 
+     
     },
 })
